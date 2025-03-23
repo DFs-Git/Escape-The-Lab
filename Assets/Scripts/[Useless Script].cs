7d93bb31e254939edc4615d2f2c985dfc1ca1796
NickelNiko <391382869@qq.com>
2025-03-23 15:44:50 +0800

    Revert "WYH 2025-03-23 15:40"
    This reverts commit 57af74f12bf699a7351fd4baa150a3feeccc5466.

--- Assets/Scripts/[Useless Script].cs
+++ Assets/Scripts/[Useless Script].cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UselessScript : MonoBehaviour
+public class [UselessScript] : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
