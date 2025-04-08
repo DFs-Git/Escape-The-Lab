a49604642f85f0fd9106a6a0bd6881182a120d9b
3174378573xiao <140292094+3174378573xiao@users.noreply.github.com>
2025-04-08 23:44:14 +0800

    xbw updata 2025/4/8 23：44
    重置按钮

---
+++ Assets/Scripts/Level/ResetButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetButton : MonoBehaviour
+{
+    public void Cliked()
+    {
+        // 获取当前场景的索引并重新加载
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
