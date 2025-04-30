ebfac617dff6453e1a65377b7d4d228645716788
DFs-Git <fishlittle2022@163.com>
2025-04-30 20:36:20 +0800

    update
    现在点击重置按钮有完整的淡入淡出动画。

--- Assets/Scripts/Level/ResetButton.cs
+++ Assets/Scripts/Level/ResetButton.cs
@@ -5,11 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class ResetButton : MonoBehaviour
 {
+    public Mask mask;
+
+    void Awake()
+    {
+        // 获取Mask
+        mask = GameObject.Find("Mask").GetComponent<Mask>();
+    }
+
     public void Cliked()
     {
         //ReactionPool.MolChemicalsInReactionPool.Clear();
         // 获取当前场景的索引并重新加载
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(currentSceneIndex);
+        StartCoroutine(mask.MaskFadeIn(currentSceneIndex));
+        // SceneManager.LoadScene(currentSceneIndex);
     }
 }
